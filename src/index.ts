9f9b8f8da7b1c08d5aa6ee19d7614c1569d43f6e
Pedro Morim <1180798@isep.ipp.pt>
2022-02-12 14:51:17 +0000

    feat: :sparkles: added array of 5-letter words

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+
+const pathToWords = resolve(__dirname, 'assets/words.txt');
+const words = readFileSync(pathToWords).toString('utf-8').split('\n');
+console.log(words);
